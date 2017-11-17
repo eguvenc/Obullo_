681ed82d1fa723d5f2dd8b62e717b618c9fb17dd
Ersin Güvenç <eguvenc@gmail.com>
2017-11-17 21:50:14 +0300

    added log folder

--- src/AppBundle/Command/HttpLogCommand.php
+++ src/AppBundle/Command/HttpLogCommand.php
@@ -30,7 +30,7 @@ class HttpLogCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $file  = ROOT . 'data/http.log';
+        $file  = ROOT . 'log/http.log';
         $clear = $input->getArgument('clear');
 
         /**
